5ac799ee23892fc6c2b0dae6b28af517e4ec7f29
gowtham-1707 <ma24c051@smail.iitm.ac.in>
2026-05-19 16:24:30 +0530

    adding files

--- model.py
+++ model.py
@@ -150,8 +150,8 @@ class Decoder(nn.Module):
 class Transformer(nn.Module):
     CHECKPOINT_URL = ""
     VOCAB_URL = ""
-    DEFAULT_CHECKPOINT_PATH = "best_checkpoint.pt"
-    DEFAULT_VOCAB_PATH = "vocab.json"
+    CHECKPOINT_URL = "https://drive.google.com/file/d/1f59ku11WPg5GJeWm9WoVUriuG8S89cWb/view?usp=sharing"
+    VOCAB_URL = "https://drive.google.com/file/d/1hTgI0G6hIYE0WWBrLaFb_W_aIEPiq2R_/view?usp=sharing"
 
     def __init__(
         self,
